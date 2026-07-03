cfa8169793c3ce6190d0a274e86c1d9a53b1bf23
blu-Encelado <bluencelado@gmail.com>
2026-07-03 15:01:26 +0200

    Input & output

--- src/repl.ts
+++ src/repl.ts
@@ -1,6 +1,30 @@
+import readline from "readline";
+
+export function startREPL() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > "
+    });
+
+    rl.prompt();
+
+    rl.on("line", (input) => {
+        const n = cleanInput(input)
+        if (n.length === 0) {
+            rl.prompt();
+        } else {
+            console.log(`Your command was: ${n[0]}`)
+            rl.prompt();
+        }
+    });
+}
+
+
 export function cleanInput(input: string): string[] {
 
     const arrayString: string[] = input.toLowerCase().trim().split(" ").filter((n) => n.length !== 0);
     
     return arrayString;
 }
+
